4bc6cace9b4a30e592777d5b3a297bbb9fc97c4e
NALANAGULA SRAVYA <75165959+mystique-sravya@users.noreply.github.com>
2023-07-14 23:17:16 +0530

    basic sample website created phase-1
    Information and Awareness: By providing a comprehensive list of orphanages categorized by type and city, your website can serve as a valuable source of information for individuals, families, and organizations seeking to support or get involved with orphanages. It raises awareness about the existence of orphanages, their locations, and the services they provide.
    
    Easy Access to Resources: Your website makes it easier for people to find orphanages near their location or in specific cities. This accessibility saves time and effort for those who are interested in volunteering, donating, or seeking assistance from orphanages.
    
    Connecting Volunteers and Donors: Your website can facilitate connections between potential volunteers, donors, and orphanages. Individuals or organizations looking to contribute their time, skills, or resources can find suitable orphanages through your website, fostering partnerships and support.
    
    Supporting Orphanages: By promoting orphanages and their work, your website can help raise awareness of their needs and challenges. This increased visibility can attract support from individuals, businesses, and philanthropic organizations, leading to increased resources, funding, and volunteer involvement for orphanages.
    
    Empowering Communities: Your website can empower local communities by highlighting the orphanages within their region. This awareness encourages community members to engage with and support these organizations, fostering a sense of social responsibility and community involvement.
    
    Advocacy and Policy Influence: Your website can play a role in advocating for the welfare and rights of orphaned children. By showcasing the work of orphanages, raising awareness of their needs, and providing information on related policies and initiatives, your website can contribute to discussions on child welfare and influence positive change.
    
    Sharing Success Stories: Your website can feature success stories and positive outcomes achieved by orphanages, highlighting the impact they have on the lives of orphaned children. These stories can inspire others to get involved, donate, or support similar initiatives.
    
    Educational Resource: Your website can serve as an educational resource, providing information about the challenges faced by orphaned children and the importance of supporting their well-being. This educational aspect can help dispel misconceptions and stereotypes surrounding orphanages and promote empathy and understanding.

---
+++ orphanagewebsite/script.js
@@ -0,0 +1,26 @@
+// Hamburger icon toggle functionality
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggler.addEventListener('click', function () {
+  navbarMenu.classList.toggle('navbar-expanded');
+});
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
